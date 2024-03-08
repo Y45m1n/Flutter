fc2ed79ec91aa8ad6037f52f7d3127355b389bdb
Y45m1n <yasmindeoliveira286@gmail.com>
2024-03-08 16:18:00 -0300

    edit

--- exemplo_mediaquery/test/widget_test.dart
+++ exemplo_mediaquery/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:exemplo_mediaquery/main.dart';
+import 'package:exemplo_mediaquery/ExemploMediaQuery.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
